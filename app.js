9a0de7ff8f3387466fe5005f6d94161769fe6c74
Serena Young <seyoung@blizzard.com>
2019-07-02 10:42:12 -0700

    Renaming assets

---
+++ app.js
@@ -0,0 +1,28 @@
+var cardBacks = [
+    {
+        "id": "default",
+        "name": "Hearthstone Default",
+        "filename": "default.png"
+    },
+    {
+        "id": "blue-portal",
+        "name": "Blue Portal",
+        "filename": "blue-portal.png"
+    },
+    {
+        "id": "monster-hunter",
+        "name":  "Monster Hunter",
+        "filename": "monster-hunter.png"
+    },
+    {
+        "id": "naxxramas",
+        "name":  "Naxxramas",
+        "filename": "naxxramas.png"
+    },
+    {
+        "id": "overwatch",
+        "name":  "Overwatch",
+        "filename": "overwatch.png"
+    }
+];
+
